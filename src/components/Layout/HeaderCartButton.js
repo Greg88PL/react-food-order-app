b45aebfec3c1faeba3b1e15dc9ca63ab641c6abe
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-10-07 12:27:13 +0200

    add context and reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,41 @@
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
+import { useContext, useEffect, useState } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = () => {
+  const [buttonIsHighlited, setButtonIsHighlited] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${styles.button} ${buttonIsHighlited ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonIsHighlited(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlited(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button}>
+    <button className={btnClasses}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
